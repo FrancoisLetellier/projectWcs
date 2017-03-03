0e98e371c618e7e445ff097481cde53c4e165353
Irena Jakubec-Berenguel <irena.jakubec@gmail.com>
2017-03-03 10:29:20 +0100

    Set height of main and footer in css

--- inc/footer.php
+++ inc/footer.php
@@ -1,9 +1,13 @@
-<div class="row">
-    <div class="footer col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        <h3>Footer</h3>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="footer col-xs-6 col-sm-6col-md-6 col-lg-6">
+            <h3>Footer</h3>
+        </div>
+        <div class="footer col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <h3>Footer</h3>
+        </div>
     </div>
-</div>
 <div>
 
 
-</div>
