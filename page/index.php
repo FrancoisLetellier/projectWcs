4a4f3b4883fb2ab4009dba893a06b0a95927fe3a
François LETELLIER <f.letellier0@gmail.com>
2017-03-08 18:03:04 +0100

    Homepage + page Devenir Franchisé, it's not responsive again

--- page/index.php
+++ page/index.php
@@ -1,6 +1,6 @@
 
 
-<div class="row">
+<div class="row ff">
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
         <div id="background-carousel">
             <img src="img/homepage.jpg" alt="restaurant lyon" >
@@ -12,25 +12,26 @@
                         <div class="row">
                             <div class="text-center col-xs-9 col-xs-offset-1 col-sm-9 col-sm-offset-1 col-md-9 col-md-offset-1
                             col-lg-9 col-lg-offset-1">
-                                <h1 class="head-restaurant">Restaurant Atypique Concept</h1>
+                                <h1 class="head-restaurant">Notre concept</h1>
                             </div>
                         </div>
                         <div class="row">
                             <div class="text-center col-xs-8 col-xs-offset-2 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2
                                 col-lg-8 col-lg-offset-2">
-                                <div class="well"><p class="description">Notre concept des restaurants atypique
+                                <div class="well"><p class="description">Notre concept
                                         consiste à vous fournir le meilleur de la gastronomie dans un cadre
-                                        le plus idilique possible.
+                                        idilique.
                                         Vous souhaitez surprendre vos proches ou vos partenaires professionnel
                                         pour leur montrer le meilleur de vous même. Les restaurants
                                         Atypique pour permettre
-                                        de réaliser vos rêves les plus vous. Depuis 1984, nous nous selectionnons
+                                        de réaliser vos rêves les plus vous. Nous selectionnons
                                         les plus fantastiques restaurant pour vous satisfaire....</p>
                                     <div class="row">
                                         <div class="col-xs-8 col-xs-offset-2 col-sm-8 col-sm-offset-2
-                                    col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
-                                        <p>
-                                        <a class="btn btn-primary btn-lg restaurant">Nos restaurants</a>
+                                        col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
+                                        <p class="fond">
+                                        <a href="/?page=restaurant" class="btn btn-primary btn-lg restaurant"
+                                           role="button">Nos restaurants</a>
                                         </p>
                                         </div>
                                     </div>
